d58136974b54b85bb519a31d56eb1bd79cf9b110
mayseoz <164212062+mayseoz@users.noreply.github.com>
2026-03-10 14:42:07 +0300

    Update MainProgram.cpp

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -4,16 +4,14 @@
 
 using namespace std;
 
-/*
----------------------------------------
-PART 1 — Variables and Arithmetic
----------------------------------------
-*/
+//*
+
+//
 
 int addNumbers(int a, int b)
 {
     // TODO
-    // Return the sum of a and b
+     return a+b;
 
 }
 
@@ -30,6 +28,12 @@ int sumUpToN(int n)
     // TODO
     // Using a loop calculate the sum
     // 1 + 2 + 3 + ... + n
+    int sum =0 ;
+    for(int i  = 1 ; i <=n ; i++ ){
+        sum += i ;
+        
+    }
+    return sum ;
 }
 
 
@@ -44,6 +48,15 @@ int findMaximum(vector<int> numbers)
 {
     // TODO
     // Return the largest number in the vector
+    int maxValue = numbers[0];
+    for ( int i=1 ; i <numbers.size (); i++ ){
+        if (numbers [i] > maxValue ){
+            maxValue = numbers [i] ;
+            
+        }
+    }
+    return maxValue;
+    
 }
 
 
@@ -58,9 +71,13 @@ string reverseString(string text)
 {
     // TODO
     // Return the reversed version of the string
+    string reversed = "";
+    for ( int i =text.length() -1 ; i >= 0 ; i--){
+    reversed += text [i];
+    
 }
-
-
+return reversed;
+}
 
 /*
 ---------------------------------------
@@ -79,6 +96,11 @@ public:
     // TODO
     // Create a constructor that receives
     // name and grade
+    Student( string n ,int g ){
+        name =n ;
+        grade=g ;
+        
+    }
 
 
     // TODO
@@ -86,6 +108,12 @@ public:
     // that prints:
     // Name: <name>
     // Grade: <grade>
+   void printInfo(){
+       cout <<"name "<< name << endl;
+       cout<< "grade"<< grade<<endl;
+       
+        
+    }
 
 };
 
